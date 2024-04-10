025b1cadf0f5d58e87deed55a696514c7442db3e
Jhonatan <jhonatan.argotydiaz@mercadolibre.com.co>
2024-04-10 09:21:22 -0500

    update

--- src/App.js
+++ src/App.js
@@ -3,13 +3,31 @@ import React from "react";
 import "./App.scss";
 import { ContextoPippoProvider } from "./ContextoPippo";
 import RoutesJS from "./Routes";
+import { isMobile } from "react-device-detect";
+import { icons } from "./pages/icons";
 
 function App() {
   return (
     <div className="main-content">
-      <ContextoPippoProvider>
-        <RoutesJS />
-      </ContextoPippoProvider>
+      {isMobile ? (
+        <div className="is_mobile">
+          <div className="img-logo">{icons("logo")}</div>
+          <p>¡Bienvenido a Pippo!</p>
+          <p>
+            Para una mejor experiencia, por favor descarga nuestra aplicación.
+          </p>
+          <p>
+            <a href="https://pippo-test.000webhostapp.com/app_pippo.zip">
+              Haz click aqui
+            </a>
+            .
+          </p>
+        </div>
+      ) : (
+        <ContextoPippoProvider>
+          <RoutesJS />
+        </ContextoPippoProvider>
+      )}
     </div>
   );
 }
